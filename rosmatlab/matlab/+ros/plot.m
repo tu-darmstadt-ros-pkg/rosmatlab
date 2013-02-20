6cd75de04a2bd03fe51f02a02487ced3f0598c47
Johannes Meyer <meyer@fsr.tu-darmstadt.de>
2013-02-20 16:20:16 +0100

    rosmatlab: updated plot script to support synchronous plotting of multiple fields

--- rosmatlab/matlab/+ros/plot.m
+++ rosmatlab/matlab/+ros/plot.m
@@ -3,24 +3,39 @@ function sub = plot(topic, datatype, field, varargin)
 %   Detailed explanation goes here
 
 % convert varargin to args struct and plot options
-args = struct('Field', field);
+args = struct('Field', {field});
 plot_options = {};
-i = 1; if mod(i,2) ~= 0; plot_options = varargin(1); i = 2; end
+i = 1;
+if mod(length(varargin),2) ~= 0
+    plot_options = varargin(1);
+    i = 2;
+end
+
 while i <= length(varargin)
     if strcmp(varargin{i}, 'Period')
         args.Period = varargin{i+1};
     else
-        plot_options = { plot_options(:), varargin(i:i+1) };
+        plot_options = { plot_options{:}, { varargin{i} }, varargin{i+1} };
+        if (~iscell(plot_options{end})); plot_options{end} = { plot_options{end} }; end
     end
     i = i + 2;
 end
 
-sub = ros.Subscriber(topic, datatype, 10);
-args.PlotObject = plot(0,0, plot_options{:});
-set(args.PlotObject, 'XData', [], 'YData', []);
+if (~iscell(args.Field)) args.Field = { args.Field }; end
+args.n = length(args.Field);
 
-if isempty(get(args.PlotObject, 'DisplayName'))
-    set(args.PlotObject, 'DisplayName', field);
+sub = ros.Subscriber(topic, datatype, 10);
+hold on;
+colororder = get(gca, 'ColorOrder');
+for i = 1:args.n
+    args.PlotObject(i) = plot(nan, nan, 'Color', colororder(i,:));
+    if isempty(get(args.PlotObject(i), 'DisplayName'))
+        set(args.PlotObject(i), 'DisplayName', args.Field{i});
+    end
+end
+
+if ~isempty(plot_options)
+    set(args.PlotObject, plot_options{:});
 end
 
 sub.UserData = args;
@@ -47,19 +62,26 @@ end
 if t == 0
     t = sub.getReceiptTime();
 end
-x = eval(['event.message.' field]);
 
-% update plot data
-xdata = [get(obj, 'XData') t];
-ydata = [get(obj, 'YData') x];
-if isfield(sub.UserData, 'Period')
-    ydata(xdata < xdata(end) - sub.UserData.Period) = [];
-    xdata(xdata < xdata(end) - sub.UserData.Period) = [];
+xdata = get(obj, 'XData')';
+ydata = get(obj, 'YData')';
+
+x = nan(length(field), 1);
+for i = 1:sub.UserData.n
+    xdata{i} = [xdata{i} t];
+    x(i) = eval(['event.message.' field{i}]);
+    ydata{i} = [ydata{i} x(i)];
+
+    % update plot data
+    if isfield(sub.UserData, 'Period')
+        ydata{i}(:,xdata{i} < xdata{i}(end) - sub.UserData.Period) = [];
+        xdata{i}(:,xdata{i} < xdata{i}(end) - sub.UserData.Period) = [];
+    end
 end
-set(obj, 'XData', xdata, 'YData', ydata);
+set(obj, {'XData'}, xdata', {'YData'}, ydata');
 
 % set XLim property
-ax = get(obj, 'Parent');
+ax = get(obj(1), 'Parent');
 xlim = get(ax, 'XLim');
 if xlim(2) < t
     xlim(2) = t;
